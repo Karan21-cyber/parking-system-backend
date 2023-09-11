8484039cd1b5ec5b2f253aa23cd92c9b05ed5601
Karan Chaudhary <tharukaran930@gmail.com>
2023-09-12 01:29:32 +0545

    update change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const dbconnect = require("../db/config");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const dbconnect = require("../db/config");
 
 const app = express();
 
@@ -15,6 +15,7 @@ const bookingRoutes = require("../Routes/bookingRouter");
 app.use(express.json());
 app.use(cors());
 
+// Define your routes
 app.use("/api/user", userRoutes);
 app.use("/api/location", locationRoutes);
 app.use("/api/space", spaceRoutes);
@@ -22,8 +23,20 @@ app.use("/api/reserve", reserveRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/booking", bookingRoutes);
 
-// app.listen(5000,() => console.log("Server running in port 5000."));
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
 
-app.use("/.netlify/functions/server", serverless(app));
+// Connect to the database
+dbconnect()
+  .then(() => {
+    console.log("Connected to the database.");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 
+// Use serverless to export the app as a serverless function
 module.exports.handler = serverless(app);
